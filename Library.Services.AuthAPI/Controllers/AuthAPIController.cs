ae6dcb903284105d676504798a7c12ae77e3d8bf
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-04 09:01:20 +0500

    Create AuthAPIController.cs and methods ('login', 'register')

---
+++ Library.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+
+        // Register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        // Login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();  
+        }
+    }
+}
