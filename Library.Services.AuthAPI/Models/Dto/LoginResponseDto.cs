5e237e266fa3e4272de75b9ea859ab6d5638dfdd
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-04 09:16:57 +0500

    Create LoginRequestDto [username, password], LoginResponseDto [user, token], RegistrationRequestDto [email, name, phoneNumber, password]

---
+++ Library.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
