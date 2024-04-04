0ecb22ddabaec6f17433b0d42181c30b000f0331
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-04 09:48:59 +0500

    Create I/AuthService [Login() and Register() methods]

---
+++ Library.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using Library.Services.AuthAPI.Models.Dto;
+using Library.Services.AuthAPI.Service.IService;
+
+namespace Library.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
