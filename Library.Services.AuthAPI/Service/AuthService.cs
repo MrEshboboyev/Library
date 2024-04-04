4a9fe00022bd3c672ce81b8cbbc07a9c61f8b29a
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-04 10:20:15 +0500

    DI in AuthService : AppDbContext, UserManager, RoleManager

--- Library.Services.AuthAPI/Service/AuthService.cs
+++ Library.Services.AuthAPI/Service/AuthService.cs
@@ -1,10 +1,26 @@
-﻿using Library.Services.AuthAPI.Models.Dto;
+﻿using Library.Services.AuthAPI.Data;
+using Library.Services.AuthAPI.Models;
+using Library.Services.AuthAPI.Models.Dto;
 using Library.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
 
 namespace Library.Services.AuthAPI.Service
 {
     public class AuthService : IAuthService
     {
+        private readonly AppDbContext _db; 
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             throw new NotImplementedException();
