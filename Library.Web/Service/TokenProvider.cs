b436f485dec7e8f9d3090178c5305b04e90dfd9d
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-20 12:03:09 +0500

    Methods implement  in TokenProvider.cs;

--- Library.Web/Service/TokenProvider.cs
+++ Library.Web/Service/TokenProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Web.Service.IService;
+using Library.Web.Utility;
 
 namespace Library.Web.Service
 {
@@ -13,17 +14,19 @@ namespace Library.Web.Service
 
         public void ClearToken()
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
         }
 
         public string? GetToken()
         {
-            throw new NotImplementedException();
+            string? token = null;
+            bool? hasToken = _contextAccessor.HttpContext?.Request.Cookies.TryGetValue(SD.TokenCookie, out token);
+            return hasToken is true ? token : null;
         }
 
         public void SetToken(string token)
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
         }
     }
 }
