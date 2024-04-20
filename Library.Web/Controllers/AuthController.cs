d1d266a0c53ee24dc498dab75e746cf9f52fa603
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-20 10:22:33 +0500

    Register() action [HttpPost] writed : registration user, and assigning Role

--- Library.Web/Controllers/AuthController.cs
+++ Library.Web/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Web.Models;
+using Library.Web.Service.IService;
 using Library.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -7,6 +8,12 @@ namespace Library.Web.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
 
         public IActionResult Index()
         {
@@ -20,6 +27,7 @@ namespace Library.Web.Controllers
             return View(loginRequestDto);
         }
 
+        #region Register actions
         [HttpGet]
         public IActionResult Register()
         {
@@ -32,6 +40,39 @@ namespace Library.Web.Controllers
             ViewBag.RoleList = roleList;
             return View();
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if(!string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+
+                assignRole = await _authService.AssignRoleAsync(obj);
+
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration successfully!";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>() 
+            {
+                new SelectListItem {Text = SD.RoleAdmin, Value = SD.RoleAdmin},
+                new SelectListItem {Text = SD.RoleCustomer, Value = SD.RoleCustomer}
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(obj);
+        }
+        #endregion
 
         [HttpGet]
         public IActionResult AssignRole()
