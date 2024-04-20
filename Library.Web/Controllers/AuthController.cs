0086d66a7e41628c4b3a2f2a8c3938ad472ef5fb
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-20 08:38:43 +0500

    Create AuthController.cs; IsAuthenticated condition in _Layout.cshtml;

---
+++ Library.Web/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Library.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AssignRole()
+        {
+            return View();
+        }
+        
+    }
+}
