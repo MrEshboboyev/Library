1252942fdb072c76183a34e9225ce1f7244d296e
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-21 16:47:58 +0500

    Generate JWT Token with user roles : add range claims for role in roles in JwtTokenGenerator.cs

--- Library.Services.AuthAPI/Service/AuthService.cs
+++ Library.Services.AuthAPI/Service/AuthService.cs
@@ -38,7 +38,8 @@ namespace Library.Services.AuthAPI.Service
             }
 
             // if user was found, Generate JWT Token
-            var token = _jwtTokenGenerator.GenerateToken(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = _jwtTokenGenerator.GenerateToken(user, roles);
 
             // create UserDto for return loginResponseDto
             UserDto userDto = new()
--- Library.Services.AuthAPI/Service/JwtTokenGenerator.cs
+++ Library.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -18,7 +18,7 @@ namespace Library.Services.AuthAPI.Service
             _jwtOptions = jwtOptions.Value;
         }
 
-        public string GenerateToken(ApplicationUser applicationUser)
+        public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
 
@@ -30,6 +30,8 @@ namespace Library.Services.AuthAPI.Service
                 new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
                 new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName),
             };
+
+            claimList.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role));
 
             var tokenDescriptor = new SecurityTokenDescriptor()
             {
