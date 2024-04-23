4250f4bde3f8f737bdd12e3a59a2112a485a9d6f
MrEshboboyev <mreshboboyev@gmail.com>
2024-04-23 14:49:02 +0500

    Data Annotations ['Required'] in LoginRequestDto.cs and RegistrationRequestDto.cs

--- Library.Web/Models/LoginRequestDto.cs
+++ Library.Web/Models/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Library.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Web.Models
 {
     public class LoginRequestDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Library.Web/Models/RegistrationRequestDto.cs
+++ Library.Web/Models/RegistrationRequestDto.cs
@@ -1,10 +1,16 @@
-﻿namespace Library.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Web.Models
 {
     public class RegistrationRequestDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public string Password { get; set; }
         public string? Role { get; set; }
     }
